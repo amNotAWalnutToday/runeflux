49b8457d711522bee4eacfaad9255071adedc7da
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2023-09-01 15:02:23 +0100

    Add basic start data to load game

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,4 +1,58 @@
+import { useEffect, useReducer } from 'react';
+import GameSchema from '../schemas/gameSchema';
+import CardSchema from '../schemas/cardSchema';
+import PlayerSchema from '../schemas/playerSchema';
+import startDeckData from '../data/start_deck.json';
+import startRuleData from '../data/start_rules.json';
+
+const enum REDUCER_ACTIONS {
+    RULE_CHANGE__DRAW,
+    RULE_CHANGE__PLAY,
+    RULE_CHANGE__KEEPER_LIMIT,
+    RULE_CHANGE__HAND_LIMIT,
+    RULE_CHANGE__LOCATION,
+    RULE_CHANGE__TELEBLOCK,
+}
+
+type TABLE_ACTIONS = {
+    type: REDUCER_ACTIONS
+    payload?: number,
+}
+
+const tableReducer = (state: GameSchema | unknown, action: TABLE_ACTIONS) => {
+    switch(action.type) {
+        case REDUCER_ACTIONS.RULE_CHANGE__DRAW: 
+            return Object.assign({}, state, {rules: {drawAmount: action.payload, playAmount: 1, keeperLimit: 2, handLimit: 5, location: 'MISTHALIN', teleblock: false}})
+        default: 
+            return state;
+    }
+}
+
+const loadGame = () => {
+    const game = {
+        deck: {
+            pure: [] as unknown[],
+            discard: [] as unknown[],
+        },
+        players: [] as PlayerSchema[],
+        goal: [] as CardSchema[],
+        round: 0,
+    }
+    for(const card of startDeckData.startDeck) {
+        game.deck.pure.push(card);
+    }
+    return Object.assign({}, game, {rules: startRuleData});
+}
+
 export default function Game() {
+    const [table, dispatchTable] = useReducer(tableReducer, loadGame());
+    
+    useEffect(() => {
+        console.log(table);
+        // setTimeout(() => dispatchTable({type: REDUCER_ACTIONS.RULE_CHANGE__DRAW, payload: Math.random() * 10}), 3000)
+    }, [table])
+
+
     return(
         <div>
             
