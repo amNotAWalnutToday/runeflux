f4ea781918b47064b544c8384b3b8232b666fdb8
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2023-09-15 18:29:07 +0100

    Update appearance of lobby

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -13,10 +13,12 @@ export default function Lobby() {
 
     const [rooms, setRooms] = useState<RoomSchema[]>([]);
     const [isAllReady, setIsAllReady] = useState(false);
+    const [refreshCD, setRefreshCD] = useState(false);
+    const [isPlayerReady, setIsPlayerReady] = useState(false);
 
     useEffect(() => {
         if(!user) return navigate('/');
-        setTimeout(() => {
+        if(!rooms.length) setTimeout(() => {
             getRooms(db, user?.uid ?? '', setRooms, setJoinedGameID);
         }, 500);
         if(joinedGameID && user) connectRoom(db, joinedGameID, user, setRooms, setJoinedGameID, setIsAllReady);
@@ -29,18 +31,25 @@ export default function Lobby() {
         /*eslint-disable-next-line*/
     }, [isAllReady]);
 
+    const getCurrentRoom = () => {
+        for(const room of rooms) {
+            if(room.id === joinedGameID) return room;
+        }
+        return null;
+    }
+
     const mapRooms = () => {
         return rooms.map((room, ind) => {
             return (
                 <div 
                     key={`room_${ind}`}
-                    className='menu_link' 
+                    className='room_link' 
                     onClick={() => {
                         joinRoom(db, room.id, user ?? {} as UserSchema, setJoinedGameID, setRooms, setIsAllReady);
                     }}
                 >
-                    <p>{room.id}</p>
-                    <p>{Array.isArray(room.game.players) && room.game.players.length} / 4</p>
+                    <p>{room.users[0].username}</p>
+                    <p>{Array.isArray(room.game.players) && room.game.players.length} / 4 Players</p>
                 </div>
             )
         });
@@ -58,12 +67,12 @@ export default function Lobby() {
                     key={`lobby_player__${ind}`}
                     className='lobby_player'
                 >
-                    <p>{ind + 1}). {player.username} {player.isReady ? 'Ready' : 'Waiting' }</p>
+                    <p>{ind + 1}). {player.username} {player.isReady ? <span className='check ready_mark' /> : <span className='cross ready_mark'/> }</p>
                     {
                     user?.uid === player.uid
                     &&
                     <button
-                        className='menu_link'
+                        className='lobby_link'
                         onClick={() => {
                             if(!user) return;
                             if(user.uid === joinedGameID) destroyRoom(db, joinedGameID, setJoinedGameID);
@@ -80,12 +89,32 @@ export default function Lobby() {
     
     return !joinedGameID ? (
         <div className='main_menu'>
-            <Link to='/game'>Join?</Link>
             <UserAccountBox />
-            <div className='menu'>
-                { mapRooms() }
+            <div className='menu lobby'>
+                <div className='refresh_btn__container' >
+                    <button
+                        className={`refresh_btn ${refreshCD ? "disabled" : ""}`}
+                        onClick={() => {
+                            if(!user || refreshCD) return;
+                            getRooms(db, user?.uid, setRooms, setJoinedGameID);
+                            setRefreshCD(() => true);
+                            setTimeout(() => setRefreshCD(() => false), 15000);
+                        }}
+                    />
+                </div>
+                { rooms.length 
+                    ? mapRooms() 
+                    : (
+                        <div>
+                            <h3 style={{textAlign: "center"}} >
+                                No Rooms
+                            </h3>
+                        </div>
+                    )
+                }
                 <button
-                    className='menu_link'
+                    style={{width: "50%", alignSelf: "center"}}
+                    className='lobby_link'
                     onClick={() => createRoom(db, user, setJoinedGameID, setRooms, setIsAllReady)}
                 >
                     Create Game
@@ -95,23 +124,18 @@ export default function Lobby() {
     ) : (
         <div className='main_menu'>
             <UserAccountBox />
-            <div className='menu'>
-                <h2>Current Session: { joinedGameID }</h2>
+            <div className='menu lobby'>
+                <h2>Current Session: { getCurrentRoom()?.users[0].username }</h2>
                 { mapRoomPlayers() }
                 <button
-                    className='menu_link'
+                    style={{marginTop: "1rem"}}
+                    className={`lobby_link ${isPlayerReady ? "disabled" : ""}`}
                     onClick={() => {
+                        setIsPlayerReady(() => true);
                         readyUp(db, joinedGameID, user ?? {} as UserSchema, setUser);
                     }}
                 >
                     Ready
-                </button>
-                <button
-                    onClick={() => {
-                        console.log(rooms);
-                    }}
-                >
-                    click
                 </button>
             </div>
         </div>
